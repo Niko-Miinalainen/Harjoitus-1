b1552540e288714191878b3fd498dfef1a808a84
Niko-Miinalainen <niko.miinalainen@edu.keuda.fi>
2024-09-18 12:01:12 +0300

    wadasd

--- tehtäviä1/tehtäviä.js
+++ tehtäviä1/tehtäviä.js
@@ -127,3 +127,37 @@ function suurempiKuin(a, b){
 suurempiKuin(20, 30)
 suurempiKuin(30, 30)
 suurempiKuin(30, 20)
+
+function parillinen(luku){
+    if (luku % 2 == 0) {
+        console.log("Luku on parillinen")
+    }
+    else {
+        console.log("Luku on pariton")
+    }
+}
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var bigNumbers = numbers.filter(numbers => numbers > 5);
+console.log(bigNumbers);
+
+function kolmioLaskuri(a, b , c){
+    if (a + b + c == 180) {
+        console.log("Kolmio")
+    }
+    else{
+        console.log("Kolmio ei mahdollinen")
+    }
+
+}
+
+function parillinenPariton(luku) {
+    if (luku % 2 == 0) {
+        console.log("Luku on parillinen");
+    }
+    else {
+        console.log("Luku on pariton");
+    }
+}
+parillinenPariton(18)
+parillinenPariton(71)
