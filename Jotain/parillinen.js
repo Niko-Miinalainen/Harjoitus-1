7f7b78a5f23977e5139773e225ddc43a3fdd5554
Niko-Miinalainen <niko.miinalainen@edu.keuda.fi>
2024-10-02 12:29:19 +0300

    2qewqw

--- Jotain/parillinen.js
+++ Jotain/parillinen.js
@@ -2,9 +2,9 @@ function parillinen(){
     let numero = document.getElementById("numero").value
     document.write("Antamasi numero: " + numero)
     if (numero%2 == 0){
-        document.write(" Antamasi numero on parillinen")
+        document.write(" on parillinen")
     }
     else{
-        document.write(" Antamasi numero on pariton")
+        document.write(" on pariton")
     }
 }
